4847228c35f8b1bda138bae4389a5e74914d6eb1
szymonqqaz <szymonqqaz22@gmail.com>
2021-01-21 01:03:05 +0100

    Add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import apiRouters from 'api';
 
@@ -10,6 +11,14 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: 'http://127.0.0.1:5500',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(apiRouters);
 
 app.listen(port, () => {
